13d4bcb41282281046393568d121bba59dc24ac6
Rohit <rohitkoppalkar@gmail.com>
2018-02-16 14:09:23 -0600

    first commit

---
+++ Chapter1/Calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCsharp6.Chapter1
+{
+    // The C# calculator.
+    class Calc
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    // This class contains the app's entry point.
+    class Program
+    {
+        static void Main()
+        {
+            Calc c = new Calc();
+            int ans = c.Add(10, 84);
+            Console.WriteLine("10 + 84 is {0}.", ans);
+            // Wait for user to press the Enter key before shutting down.
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ Chapter1/Hello.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCsharp6.Chapter1
+{
+    class Hello
+    {
+        static int Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
